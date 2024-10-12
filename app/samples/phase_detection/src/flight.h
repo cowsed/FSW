a28962b466f778717ff7166bce464f8de2696a7e
cowsed <richie.sommers@outlook.com>
2024-10-11 23:20:17 -0400

    yappin about the flight

--- app/samples/phase_detection/src/flight.h
+++ app/samples/phase_detection/src/flight.h
@@ -18,6 +18,7 @@ enum Sources : uint8_t {
     IMU1,
     IMU2,
     Barom1,
+    NoseoverLockout,
     Boost2CoastTimer,
     Noseover2MainTimer,
     FullFlightTimer,
@@ -25,35 +26,48 @@ enum Sources : uint8_t {
     NumSources
 };
 constexpr std::array<const char *, Sources::NumSources> sourceNames = {
-    "IMU 1 (LSM6DSL)",     "IMU 2 (ADXL375)",   "Barom 1 (BME280)",  "Boost2Coast Timer",
-    "Noseover2Main Timer", "Full Flight Timer", "ExtraCamera Timer",
+    "IMU 1 (LSM6DSL)",   "IMU 2 (ADXL375)",     "Barom 1 (BME280)",  "Noseover Lockout",
+    "Boost2Coast Timer", "Noseover2Main Timer", "Full Flight Timer", "ExtraCamera Timer",
 };
 
-constexpr std::size_t num_timer_events = 4;
+constexpr std::size_t num_timer_events = 5;
 using Controller = PhaseController<Events, Events::NumEvents, Sources, Sources::NumSources, num_timer_events>;
 
 /**
  * Special events triggered not by sensors but by timers between phases
  */
 constexpr std::array<Controller::TimerEvent, num_timer_events> timer_events = {
+    // The engine should burn for around 3 seconds. don't detect coast unless the engine has been firing for a bit
     Controller::TimerEvent{
         .start = Events::Boost,
         .event = Events::Coast,
         .time = K_SECONDS(3),
         .source = Sources::Boost2CoastTimer,
     },
+    // We dont want to accidentally detect noseover when still burning or while going really fast.
+    // Adds a lockout timer so even if our sensors say we nosed over, don't trust them until we've had time to slowdown
+    Controller::TimerEvent{
+        .start = Events::Boost,
+        .event = Events::Noseover,
+        .time = K_SECONDS(5),
+        .source = Sources::NoseoverLockout,
+    },
+    // Rather than deploying chutes at an altitude, deploy them at a certain time after noseover
     Controller::TimerEvent{
         .start = Events::Noseover,
         .event = Events::MainChute,
         .time = K_SECONDS(5),
         .source = Sources::Noseover2MainTimer,
     },
+    // We know our entire flight will not last longer than X seconds even if we main at apogee.
+    //This stops us from overwriting flight data if we don't detect the end
     Controller::TimerEvent{
         .start = Events::Boost,
         .event = Events::GroundHit,
         .time = K_SECONDS(10),
         .source = Sources::FullFlightTimer,
     },
+    // After we hit the ground, keep the cameras going for a while longer so they start a new video file and when we cut the power, no actual flight footage is lost
     Controller::TimerEvent{
         .start = Events::GroundHit,
         .event = Events::CamerasOff,
@@ -74,16 +88,21 @@ constexpr std::array<Controller::DecisionFunc, Events::NumEvents> deciders = [] 
     arr[Events::PadReady] = [](Controller::SourceStates states) -> bool {
         return states[Sources::IMU1] && states[Sources::IMU2] && states[Sources::Barom1];
     };
+
     // Boosting
     arr[Events::Boost] = [](Controller::SourceStates states) -> bool {
         return (states[Sources::IMU1] && states[Sources::IMU2]) || states[Sources::Barom1];
     };
+
     // Coasting
     arr[Events::Coast] = [](Controller::SourceStates states) -> bool {
         return states[Sources::Boost2CoastTimer] || states[Sources::IMU1] || states[Sources::IMU2];
     };
+
     // Noseover
-    arr[Events::Noseover] = [](Controller::SourceStates states) -> bool { return states[Sources::Barom1]; };
+    arr[Events::Noseover] = [](Controller::SourceStates states) -> bool {
+        return states[Sources::NoseoverLockout] && states[Sources::Barom1];
+    };
 
     // Main
     arr[Events::MainChute] = [](Controller::SourceStates states) -> bool {
@@ -94,6 +113,7 @@ constexpr std::array<Controller::DecisionFunc, Events::NumEvents> deciders = [] 
     arr[Events::GroundHit] = [](Controller::SourceStates states) -> bool {
         return states[Sources::Barom1] || states[Sources::FullFlightTimer];
     };
+
     // After finishing stuff up (what grim did to make sure camera SD cards fully saved)
     arr[Events::CamerasOff] = [](Controller::SourceStates states) -> bool { return states[Sources::ExtraCameraTimer]; };
 
